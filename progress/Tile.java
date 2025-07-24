3ba7b0d0fcc8fbd0c164a5061b6715f2a367c295
mikagenosuka <dya110206@gmail.com>
2025-07-25 01:09:58 +0800

    added getters for Zombies & Plants

--- progress/Tile.java
+++ progress/Tile.java
@@ -1,17 +1,44 @@
-public class Tile { 
-	public Tile (int row, int col) {
-		this.row = row;
-		this.col = col;
-	}
-	
-	public int getRow() {//?
-		return row;
-	}
+import java.util.ArrayList;
+import java.util.List;
 
-	public int getCol() {//?
-		return col;
-	}
+public class Tile {
+    private Plant plant;
+    private final List<Zombie> zombies;
 
-	private int row;
-	private int col;
+    public Tile() {
+        this.plant = null;
+        this.zombies = new ArrayList<>();
+    }
+
+    public Plant getPlant() {
+        return plant;
+    }
+
+    public void setPlant(Plant plant) {
+        this.plant = plant;
+    }
+
+    public boolean hasPlant() {
+        return plant != null && !plant.isDead();
+    }
+
+    public void removePlant() {
+        this.plant = null;
+    }
+
+    public List<Zombie> getZombies() {
+        return zombies;
+    }
+
+    public void addZombie(Zombie z) {
+        zombies.add(z);
+    }
+
+    public void removeZombie(Zombie z) {
+        zombies.remove(z);
+    }
+
+    public boolean hasZombies() {
+        return !zombies.isEmpty();
+    }
 }
